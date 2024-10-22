7aa74d6a2bf59953c6855c1bdd69d6e4d3c98fbf
Ravshan Yuldashev <38344113+rorua@users.noreply.github.com>
2024-10-22 11:50:59 +0500

    Fix params opts. Filed example (#2)
    * Fixed logger provider opt

--- examples/main.go
+++ examples/main.go
@@ -1,18 +1,42 @@
 package main
 
-//import "github.com/rorua/otelemetry"
+import (
+	"context"
+
+	"github.com/rorua/otelemetry"
+	"go.opentelemetry.io/otel/metric"
+)
 
 func main() {
-	// This is a placeholder for the main function.
-	//telemetry := otelemetry.New(otelemetry.Config{
-	//	Service: otelemetry.Service{
-	//		Name:      "my-service",
-	//		Namespace: "my-namespace",
-	//		Version:   "1.0.0",
-	//	},
-	//	Collector: otelemetry.Collector{
-	//		Host: "localhost",
-	//		Port: "4317",
-	//	},
-	//})
+
+	ctx := context.Background()
+
+	cfg := otelemetry.Config{}
+	cfg.Service.Name = "my-service"
+	cfg.Service.Namespace = "my-namespace"
+	cfg.Service.Version = "1.0.0"
+	cfg.Collector.Host = "0.0.0.0"
+	cfg.Collector.Port = "4317"
+
+	//This is a placeholder for the main function.
+	telemetry, err := otelemetry.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	defer telemetry.Shutdown(ctx)
+
+	ctx, span := telemetry.StartSpan(ctx, "main")
+	defer span.End()
+
+	// Do something with telemetry
+	telemetry.Log().Info(ctx, "hello world")
+
+	requestCount, err := telemetry.Meter().Int64Counter(
+		"example_counter",
+		metric.WithDescription("The number of requests received"),
+	)
+
+	requestCount.Add(ctx, 1)
+
 }
